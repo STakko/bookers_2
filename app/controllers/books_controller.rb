9370cf5ac6345c425516465f98961083800780e3
EC2 Default User <ec2-user@ip-172-31-86-126.ec2.internal>
2021-11-02 13:10:17 +0000

    [Add]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,18 +8,30 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path
+      redirect_to book_path(@book)
     else
       @books = Book.all
       render:index
     end
   end
   
+  def show
+    @book = Book.find(params[:id])
+  end
+  
   def index
     @books = Book.all
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
     
-    private
+  private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
